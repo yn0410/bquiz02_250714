7fdadcea652113995352e8e6b9ae87e45ac83eff
yn <jenny783113@gmail.com>
2025-07-14 15:19:47 +0800

    完成忘記密碼功能

--- front/forgot.php
+++ front/forgot.php
@@ -4,7 +4,16 @@
     <div>
         <input type="text" name="email" id="email">
     </div>
+    <div id="answer"></div>
     <div>
-        <button>尋找</button>
+        <button onclick="search()">尋找</button>
     </div>
 </fieldset>
+
+<script>
+    function search(){
+        $.get("./api/chk_email.php",{email:$("#email").val()}, (res)=>{
+            $("#answer").html(res);
+        })
+    }
+</script>
