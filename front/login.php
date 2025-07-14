cb7e4c0c4e28733aeb62c1a71bdefb8758028a83
yn <jenny783113@gmail.com>
2025-07-14 11:51:31 +0800

    建立會員註冊、登入、忘記密碼頁面

---
+++ front/login.php
@@ -0,0 +1,29 @@
+<fieldset>
+    <legend>會員登入</legend>
+
+    <form>
+        <table>
+            <tr>
+                <td>帳號</td>
+                <td><input type="text" name="acc" id="acc"></td>
+            </tr>
+            <tr>
+                <td>密碼</td>
+                <td>
+                    <input type="text" name="pw" id="pw">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="button" value="登入">
+                    <input type="reset" value="清除">
+                </td>
+                <td>
+                    <a href="?do=forgot">忘記密碼</a>
+                    |
+                    <a href="?do=reg">尚未註冊</a>
+                </td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
