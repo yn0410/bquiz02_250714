7e6d41412316abe0116e7d30f360d3d5816e6aa4
yn <jenny783113@gmail.com>
2025-07-15 13:38:36 +0800

    完成(?)分類網誌功能

---
+++ api/get_post.php
@@ -0,0 +1,7 @@
+<?php include_once "db.php";
+
+$post=$News->find($_GET['id']);
+echo "<h3>{$post['title']}</h3>";
+echo nl2br($post['text']); //new line to br
+
+?>
--- front/po.php
+++ front/po.php
@@ -1,5 +1,5 @@
 <div class="nav">
-    目前位置：首頁 > 分類網誌 > <span id="NavType">慢性病防治</span>
+    目前位置：首頁 > 分類網誌 > <span id="NavType">健康新知</span>
 </div>
 <fieldset style="width: 120px; display:inline-block; vertical-align:top">
     <legend>分類網誌</legend>
@@ -9,23 +9,45 @@
     <div><a class="type-link" data-type="4">慢性病防治</a></div>
 </fieldset>
 
-<fieldset style="width: 600px; display:inline-block;">
+<fieldset style="width: 590px; display:inline-block;">
     <legend>文章列表</legend>
     <div id="TypeList"></div>
     <div id="Post"></div>
 </fieldset>
 
 <script>
+    getList(1);
+
     $(".type-link").on("click", function(){
         let type=$(this).text();
         $("#NavType").text(type);
 
         let typeId=$(this).data("type");
-        $.get("./api/get_type_list.php", {type:typeId}, function(list){
+        getList(typeId);
+
+    })
+
+    function getPost(id){
+        $.get("./api/get_post.php", {id}, function(post){
+            $("#TypeList").html("");
+            $("#Post").html(post);
+        })
+    }
+
+    $(".post-item").on("click",function(){
+        let postId=$(this).data("post");
+        getPost(postId);
+    })
+
+    function getList(type){
+        $.get("./api/get_type_list.php", {type}, function(list){
             $("#Post").html("");
             $("#TypeList").html(list);
 
+            $(".post-item").on("click",function(){
+                let postId=$(this).data("post");
+                getPost(postId);
+            })
         })
-
-    })
+    }
 </script>
