d0cc35c1c97912a849bbdb6d47d5c9260bb0ffe1
yn <jenny783113@gmail.com>
2025-07-15 11:31:11 +0800

    建立分類網誌頁面

---
+++ front/po.php
@@ -0,0 +1,14 @@
+<div class="nav">
+    目前位置：首頁 > 分類網誌 > <span id="NavType">慢性病防治</span>
+</div>
+<fieldset style="width: 120px; display:inline-block; vertical-align:top">
+    <legend>分類網誌</legend>
+    <div><a>健康新知</a></div>
+    <div><a>菸害防治</a></div>
+    <div><a>癌症防治</a></div>
+    <div><a>慢性病防治</a></div>
+</fieldset>
+
+<fieldset style="width: 600px; display:inline-block;">
+    <legend>文章列表</legend>
+</fieldset>
