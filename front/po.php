67f7f8ee86c324e3adad61f1d65177380d2f56f0
yn <jenny783113@gmail.com>
2025-07-15 11:48:34 +0800

    建立分類網誌頁面及取得文章列表功能

--- front/po.php
+++ front/po.php
@@ -3,12 +3,29 @@
 </div>
 <fieldset style="width: 120px; display:inline-block; vertical-align:top">
     <legend>分類網誌</legend>
-    <div><a>健康新知</a></div>
-    <div><a>菸害防治</a></div>
-    <div><a>癌症防治</a></div>
-    <div><a>慢性病防治</a></div>
+    <div><a class="type-link" data-type="1">健康新知</a></div>
+    <div><a class="type-link" data-type="2">菸害防治</a></div>
+    <div><a class="type-link" data-type="3">癌症防治</a></div>
+    <div><a class="type-link" data-type="4">慢性病防治</a></div>
 </fieldset>
 
 <fieldset style="width: 600px; display:inline-block;">
     <legend>文章列表</legend>
+    <div id="TypeList"></div>
+    <div id="Post"></div>
 </fieldset>
+
+<script>
+    $(".type-link").on("click", function(){
+        let type=$(this).text();
+        $("#NavType").text(type);
+
+        let typeId=$(this).data("type");
+        $.get("./api/get_type_list.php", {type:typeId}, function(list){
+            $("#Post").html("");
+            $("#TypeList").html(list);
+
+        })
+
+    })
+</script>
