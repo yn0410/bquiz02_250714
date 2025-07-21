b17a54fa649b867fd53bfdc227ad19f8a53dbd4c
yn <jenny783113@gmail.com>
2025-07-21 14:32:52 +0800

    fix(版型問題)

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 ﻿<?php include_once './api/db.php'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-<title>健康促進網</title>
-<link href="./css/css.css" rel="stylesheet" type="text/css">
-<script src="./js/jquery-1.9.1.min.js"></script>
-<script src="./js/js.js"></script>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+	<title>健康促進網</title>
+	<link href="./css/css.css" rel="stylesheet" type="text/css">
+	<script src="./js/jquery-1.9.1.min.js"></script>
+	<script src="./js/js.js"></script>
 </head>
 
 <body>
-<div id="alerr" class="pop">
-	<pre id="ssaa"></pre>
-</div>
+
 
 	<div id="all">
     	<div id="title">
